c4cceff0e93155d71ff73d612b00e61ba76e5f19
tysonbivens-design <tysonbivens@gmail.com>
2026-03-08 07:29:48 -0700

    Update PushPrompt.tsx

--- src/components/PushPrompt.tsx
+++ src/components/PushPrompt.tsx
@@ -104,5 +104,5 @@ function urlBase64ToUint8Array(base64String: string): Uint8Array {
   const padding = '='.repeat((4 - (base64String.length % 4)) % 4)
   const base64 = (base64String + padding).replace(/-/g, '+').replace(/_/g, '/')
   const rawData = window.atob(base64)
-  return new Uint8Array([...rawData].map(c => c.charCodeAt(0)))
+  return new Uint8Array(Array.from(rawData).map(c => c.charCodeAt(0)))
 }
